d742b3dfcbeb7ca45c28567a1ee5ec71b92133e7
rayhan57 <rayhanb18@gmail.com>
2023-01-03 15:41:37 +0700

    update

--- Komgraf/src/Komgraf/LatihanUPM.java
+++ Komgraf/src/Komgraf/LatihanUPM.java
@@ -26,30 +26,49 @@ public class LatihanUPM extends javax.swing.JPanel {
     public void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
 
+//      Kepala
         g2d.setColor(Color.GREEN);
         g2d.fillOval(70, 30, 150, 100);
+
+//      Kuping
         g2d.setColor(Color.BLUE);
         g2d.fillOval(55, 50, 20, 50);
         g2d.fillOval(215, 50, 20, 50);
+
+//      Mata
         g2d.setColor(Color.WHITE);
         g2d.fillOval(100, 45, 25, 25);
         g2d.fillOval(170, 45, 25, 25);
+
+//      Bola mata
         g2d.setColor(Color.BLACK);
         g2d.fillOval(105, 55, 10, 15);
         g2d.fillOval(175, 55, 10, 15);
+
+//      Hidung
         g2d.setColor(Color.RED);
         g2d.fillArc(125, 40, 50, 50, 240, 60);
+
+//      Topi
         g2d.setColor(Color.PINK);
         g2d.fillArc(100, 5, 90, 60, 0, 180);
         g2d.setColor(Color.RED);
         g2d.fillArc(75, 20, 100, 40, 20, 180);
         g2d.fillArc(115, 20, 100, 40, 340, 180);
+
+//      Mulut
         g2d.setColor(Color.ORANGE);
         g2d.fillOval(105, 100, 80, 15);
+
+//      Badan
         g2d.fillRoundRect(110, 130, 80, 50, 10, 10);
+
+//      Tangan
         g2d.setColor(Color.BLACK);
         g2d.fillOval(70, 135, 40, 15);
         g2d.fillOval(190, 135, 40, 15);
+
+//      Kaki
         g2d.fillOval(120, 180, 15, 40);
         g2d.fillOval(160, 180, 15, 40);
         g2d.fillOval(120, 210, 30, 10);
